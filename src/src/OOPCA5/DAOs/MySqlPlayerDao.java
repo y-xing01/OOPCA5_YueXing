7903033d44f4169d8a1080411351bc60d2da45c6
YueXingTong <d00234628@student.dkit.ie>
2022-03-23 23:32:29 +0000

    Implemented findPlayerByAge in MySqlPlayerDao.java

--- src/src/OOPCA5/DAOs/MySqlPlayerDao.java
+++ src/src/OOPCA5/DAOs/MySqlPlayerDao.java
@@ -8,6 +8,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class MySqlPlayerDao extends MySqlDao implements PlayerDaoInterface{
     @Override
@@ -20,13 +21,11 @@ public class MySqlPlayerDao extends MySqlDao implements PlayerDaoInterface{
 
         try
         {
-            //Get connection object using the methods in the super class (MySqlDao.java)...
             connection = this.getConnection();
 
             String query = "SELECT * FROM PLAYER";
             ps = connection.prepareStatement(query);
 
-            //Using a PreparedStatement to execute SQL...
             resultSet = ps.executeQuery();
             while (resultSet.next())
             {
@@ -62,6 +61,61 @@ public class MySqlPlayerDao extends MySqlDao implements PlayerDaoInterface{
                 throw new DaoException("Find All Players : " + e.getMessage());
             }
         }
-        return playerList;     // may be empty
+        return playerList;
+    }
+
+    @Override
+    public ArrayList<Player> findPlayerByAge(int player_age1, int player_age2) throws DaoException
+    {
+        Connection connection = null;
+        PreparedStatement ps = null;
+        ResultSet resultSet = null;
+        ArrayList<Player> playerList = new ArrayList<>();
+
+        try
+        {
+            connection = this.getConnection();
+
+            String query = "SELECT * FROM PLAYER WHERE PLAYER_AGE BETWEEN ? AND ?";
+            ps = connection.prepareStatement(query);
+            ps.setInt(1,  player_age1);
+            ps.setInt(2,  player_age2);
+
+            resultSet = ps.executeQuery();
+            while (resultSet.next())
+            {
+                int playerWorldRank = resultSet.getInt("PLAYER_WORLD_RANK");
+                String playerName = resultSet.getString("PLAYER_NAME");
+                int playerAge = resultSet.getInt("PLAYER_AGE");
+                float playerHeight = resultSet.getFloat("PLAYER_HEIGHT");
+                int playerCareerWin = resultSet.getInt("PLAYER_CAREER_WIN");
+                Player p = new Player(playerWorldRank, playerName, playerAge, playerHeight, playerCareerWin);
+                playerList.add(p);
+            }
+        } catch (SQLException e)
+        {
+            throw new DaoException("Find all Player Age between " + e.getMessage());
+        } finally
+        {
+            try
+            {
+                if (resultSet != null)
+                {
+                    resultSet.close();
+                }
+                if (ps != null)
+                {
+                    ps.close();
+                }
+                if (connection != null)
+                {
+                    freeConnection(connection);
+                }
+            } catch (SQLException e)
+            {
+                throw new DaoException("Find all Player Age between  " + e.getMessage());
+            }
+        }
+        return playerList;
     }
 }
