e03b935138bee15b2e53013f4aff58df45e39041
YueXingTong <d00234628@student.dkit.ie>
2022-04-04 00:11:43 +0100

    JUnit Test

---
+++ src/src/OOPCA5/Test/AppMenuTest.java
@@ -0,0 +1,13 @@
+package OOPCA5.Test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+class AppMenuTest {
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
